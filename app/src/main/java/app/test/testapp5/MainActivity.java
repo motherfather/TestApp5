add6283a6be3a7b5de47e54e293721de9c125744
motherfather <father@mail.com>
2017-01-20 19:02:54 +0900

    20170120 19:02 액션바 메뉴버튼 교체~

--- app/src/main/java/app/test/testapp5/MainActivity.java
+++ app/src/main/java/app/test/testapp5/MainActivity.java
@@ -3,6 +3,7 @@ package app.test.testapp5;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -45,23 +47,17 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (findViewById(R.id.fab).getVisibility() == View.VISIBLE) {
+            findViewById(R.id.fab).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.fab).setVisibility(View.VISIBLE);
         }
-
         return super.onOptionsItemSelected(item);
     }
 
